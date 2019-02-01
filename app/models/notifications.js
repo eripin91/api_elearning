f83187f2a38262b3cb8ec9a21d0dc2945897b71e
arham09 <arham.abiyan@gmail.com>
2019-02-01 14:13:00 +0700

    Add Notification Model

---
+++ app/models/notifications.js
@@ -0,0 +1,17 @@
+'use strict'
+
+module.exports = {
+  get: (conn, data, callback) => {
+    conn.getConnection((errConnection, connection) => {
+      if (errConnection) console.error(errConnection)
+
+      connection.query(`INSERT INTO notification_tab SET ?`, data, (err, rows) => {
+        if (err) {
+          callback(err)
+        } else {
+          callback(null, _.merge(data, { id: rows.insertId}))
+        }
+      })
+    })
+  }
+}
