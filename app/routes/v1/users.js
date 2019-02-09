040a024251ae612fac613aca2a3843bdc6b9f39d
Gratcy Palma <gratcypalma@gmail.com>
2019-02-09 13:34:59 +0700

    add authorization check header and validate token

--- app/routes/v1/users.js
+++ app/routes/v1/users.js
@@ -1,12 +1,13 @@
-/* global UsersControllers */
+/* global UsersControllers AuthHelper */
 
 'use strict'
 
 var Route = express.Router()
 
 Route
+  .all('/*', AuthHelper.requiresAuthorization)
   .get('/get', UsersControllers.get)
-  .get('/:userId/classes', UsersControllers.getUserClass)
+  .get('/:userId/classes', AuthHelper.requiresAuthorization, AuthHelper.requiresAccessToken, UsersControllers.getUserClass)
   .post('/login', UsersControllers.login)
   .get('/request-token', UsersControllers.requestToken)
   .get('/logout', UsersControllers.logout)
