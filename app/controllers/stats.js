daa31cb9371bfc6f527fded7902bd5481da4d05b
arham09 <arham.abiyan@gmail.com>
2019-02-11 17:21:40 +0700

    Add Certicicate List in user stat

--- app/controllers/stats.js
+++ app/controllers/stats.js
@@ -39,6 +39,30 @@ exports.get = (req, res) => {
       statsModel.getUserStatistic(req, req.params.userId, (errStats, resultStats) => {
         cb(errStats, _.result(resultStats, '[0]'))
       })
+    },
+    (dataStat, cb) => {
+      statsModel.getCertificate(req, req.params.userId, (err, result) => {
+        if (!err) {
+          const data = result.slice(0, 5)
+          dataStat.certificate_length = result.length
+          dataStat.certificate = data
+        }
+        cb(err, dataStat)
+      })
+    },
+    (dataStat, cb) => {
+      async.eachSeries(dataStat.certificate, (item, next) => {
+        statsModel.checkClass(req, item.classid, (err, result) => {
+          if (err) console.error(err)
+
+          result.map((className) => {
+            item.content = `Lulus dari kelas ${className.name}`
+          })
+          next()
+        })
+      }, err => {
+        cb(err, dataStat)
+      })
     }
   ], (errStats, resultStats) => {
     if (!errStats) {
@@ -47,4 +71,100 @@ exports.get = (req, res) => {
       return MiscHelper.errorCustomStatus(res, errStats, 400)
     }
   })
+}
+
+exports.getCertificate = (req, res) => {
+  req.checkParams('userId', 'userId is required').notEmpty().isInt()
+  req.checkParams('classId', 'classId is required').notEmpty().isInt()
+
+  if (req.validationErrors()) {
+    return MiscHelper.errorCustomStatus(res, req.validationErrors(true))
+  }
+
+  const key = `get-certificate-${req.params.userId}-${req.params.classId}`
+  async.waterfall([
+    (cb) => {
+      redisCache.get(key, detail => {
+        if (detail) {
+          return MiscHelper.responses(res, detail)
+        } else {
+          cb(null)
+        }
+      })
+    },
+    (cb) => {
+      statsModel.getUserCertificate(req, req.params.userId, req.params.classId, (errCertificate, resultCertificate) => {
+        if (_.isEmpty(resultCertificate)) {
+          resultCertificate.push({certificate: 'You are Not Passed This Class Yet'})
+          cb(errCertificate, resultCertificate[0])
+        } else {
+          cb(errCertificate, resultCertificate[0])
+        }
+      })
+    },
+    (dataCertificate, cb) => {
+      redisCache.setex(key, 600, dataCertificate)
+      console.log(`proccess cached`)
+      cb(null, dataCertificate)
+    }
+  ], (errCertificate, resultCertificate) => {
+    if (!errCertificate) {
+      return MiscHelper.responses(res, resultCertificate)
+    } else {
+      return MiscHelper.errorCustomStatus(res, errCertificate)
+    }
+  })
+}
+
+exports.getCertificateList = (req, res) => {
+  req.checkParams('userId', 'userId is required').notEmpty().isInt()
+
+  if (req.validationErrors()) {
+    return MiscHelper.errorCustomStatus(res, req.validationErrors(true))
+  }
+
+  const key = `get-certificate-${req.params.userId}`
+
+  async.waterfall([
+    (cb) => {
+      redisCache.get(key, detail => {
+        if (detail) {
+          return MiscHelper.responses(res, detail)
+        } else {
+          cb(null)
+        }
+      })
+    },
+    (cb) => {
+      statsModel.getCertificate(req, req.params.userId, (errCertificate, resultCertificate) => {
+        cb(errCertificate, resultCertificate)
+      })
+    },
+    (data, cb) => {
+      async.eachSeries(data, (item, next) => {
+        statsModel.checkClass(req, item.classid, (err, result) => {
+          if (err) console.error(err)
+
+          result.map((className) => {
+            item.content = `Lulus dari kelas ${className.name}`
+          })
+          next()
+        })
+      }, err => {
+        cb(err, data)
+      })
+    },
+    (dataCertificate, cb) => {
+      redisCache.setex(key, 600, dataCertificate)
+      console.log('proccess cached')
+      cb(null, dataCertificate)
+    }
+  ], (errList, resultList) => {
+    if (!errList) {
+      return MiscHelper.responses(res, resultList)
+    } else {
+      return MiscHelper.errorCustomStatus(res, errList)
+    }
+  })
+  
 }
--- app/routes/v1/stats.js
+++ app/routes/v1/stats.js
@@ -6,5 +6,7 @@ var Route = express.Router()
 
 Route
   .get('/get/:userId', StatsControllers.get)
+  .get('/certificate/:userId/:classId', StatsControllers.getCertificate)
+  .get('/certificate/:userId', StatsControllers.getCertificateList)
 
 module.exports = Route
