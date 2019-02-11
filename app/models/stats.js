faf93553d5c4a5ff3acbf3ec90ee7f670a203fc1
fitraaditama7 <fitraaditama7@gmail.com>
2019-02-11 17:40:37 +0700

    Revert "Merge branch 'user-stats-2' of github.com:sigmatech-eripin/e-learning into dashboard"
    This reverts commit 6daf4d2d022b1aef780061d2384737af7312ae9e, reversing
    changes made to 8bb54ed5452877672e98e1d0df126774e675df45.

--- app/models/stats.js
+++ app/models/stats.js
@@ -8,32 +8,5 @@ module.exports = {
         callback(err, rows)
       })
     })
-  },
-  getUserCertificate: (conn, userId, classId, callback) => {
-    conn.getConnection((errConnection, connection) => {
-      if (errConnection) console.error(errConnection)
-
-      connection.query(`SELECT certificate FROM users_classes_tab WHERE userid=? AND classid=? AND is_completed=1 AND status=1`, [userId, classId], (err, rows) => {
-        callback(err, rows)
-      })
-    })
-  },
-  getCertificate: (conn, userId, callback) => {
-    conn.getConnection((errConnection, connection) => {
-      if (errConnection) console.error(errConnection)
-
-      connection.query(`SELECT a.id, a.classid, a.userid, a.finished_at FROM users_classes_tab a WHERE userid=? AND is_completed=1 AND status=1`, userId, (err, rows) => {
-        callback(err, rows)
-      })
-    })
-  },
-  checkClass: (conn, classId, callback) => {
-    conn.getConnection((errConnection, connection) => {
-      if (errConnection) console.error(connection)
-
-      connection.query(`SELECT name FROM classes_tab WHERE classid=?`, classId, (err, rows) => {
-        callback(err, rows)
-      })
-    })
   }
 }
--- app/routes/v1/stats.js
+++ app/routes/v1/stats.js
@@ -6,7 +6,5 @@ var Route = express.Router()
 
 Route
   .get('/get/:userId', StatsControllers.get)
-  .get('/certificate/:userId/:classId', StatsControllers.getCertificate)
-  .get('/certificate/:userId', StatsControllers.getCertificateList)
 
 module.exports = Route
