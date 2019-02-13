68c192362cdb66d1260fa64e104621c480429a96
arham09 <arham.abiyan@gmail.com>
2019-02-13 15:00:24 +0700

    Add rank to user stat detail

--- app/controllers/stats.js
+++ app/controllers/stats.js
@@ -58,9 +58,46 @@ exports.get = (req, res) => {
           result.map((className) => {
             item.content = `Lulus dari kelas ${className.name}`
           })
+
+          let date = new Date(item.finished_at)
+          const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+          item.time = `${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`
+
           next()
         })
       }, err => {
+        cb(err, dataStat)
+      })
+    },
+    (dataStat, cb) => {
+      statsModel.getUserAchievementClass(req, req.params.userId, (err, result) => {
+        if (!err) {
+          const data = result.slice(0, 5)
+          dataStat.award_length = result.length
+          dataStat.award = data
+        }
+        cb(err, dataStat)
+      })
+    },
+    (dataStat, cb) => {
+      async.eachSeries(dataStat.award, (item, next) => {
+        console.log(dataStat.award)
+        statsModel.getUserAchievementScore(req, item.classid, (err, result) => {
+          if (err) console.error(err)
+          result.map((rank, index) => {
+            if (rank.userid === item.userid) {
+              item.message = `Peringkat ${index + 1} di Kelas ${rank.name}`
+
+              let date = new Date(rank.created_at)
+              const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+              item.time = `${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`
+            }
+          })
+          next()
+        })
+      }, err => {
+        redisCache.setex(key, 600, dataStat)
+        console.log('proccess cached')
         cb(err, dataStat)
       })
     }
@@ -68,7 +105,7 @@ exports.get = (req, res) => {
     if (!errStats) {
       return MiscHelper.responses(res, resultStats)
     } else {
-      return MiscHelper.errorCustomStatus(res, errStats, 400)
+      return MiscHelper.errorCustomStatus(res, errStats)
     }
   })
 }
@@ -110,7 +147,7 @@ exports.getCertificate = (req, res) => {
           resultCertificate.push({ message: 'You are Not Passed This Class Yet' })
           cb(errCertificate, resultCertificate[0])
         } else {
-          resultCertificate[0].certificate = ``
+          resultCertificate[0].certificate = `Sertifikat ini diberikan kepada ${resultCertificate[0].username} Sebagai bukti Kelulusan dari kelas ${resultCertificate[0].classname}`
           cb(errCertificate, resultCertificate[0])
         }
       })
@@ -172,6 +209,11 @@ exports.getCertificateList = (req, res) => {
           result.map((className) => {
             item.content = `Lulus dari kelas ${className.name}`
           })
+
+          let date = new Date(item.finished_at)
+          const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+          item.time = `${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`
+
           next()
         })
       }, err => {
@@ -188,6 +230,74 @@ exports.getCertificateList = (req, res) => {
       return MiscHelper.responses(res, resultList)
     } else {
       return MiscHelper.errorCustomStatus(res, errList)
+    }
+  })
+}
+
+/*
+ * GET : '/stats/rank/:userId'
+ *
+ * @desc Get all 'prestasi' list per user
+ *
+ * @param  {object} req - Parameters for request
+ * @param  {objectId} req.params.userId - userId
+ *
+ * @return {object} Request object
+ */
+
+exports.getRank = (req, res) => {
+  req.checkParams('userId', 'userId is required').notEmpty().isInt()
+
+  if (req.validationErrors()) {
+    return MiscHelper.errorCustomStatus(req, req.validationErrors(true))
+  }
+
+  const key = `get-user-achievement-${req.params.userId}:${new Date().getTime()}`
+
+  async.waterfall([
+    (cb) => {
+      redisCache.get(key, users => {
+        if (users) {
+          return MiscHelper.responses(res, users)
+        } else {
+          cb(null)
+        }
+      })
+    },
+    (cb) => {
+      statsModel.getUserAchievementClass(req, req.params.userId, (errClass, resultClass) => {
+        if (_.isEmpty(resultClass)) {
+          return MiscHelper.errorCustomStatus(res, { message: 'You Have No Achievement' })
+        } else {
+          cb(errClass, resultClass)
+        }
+      })
+    },
+    (dataClass, cb) => {
+      async.eachSeries(dataClass, (item, next) => {
+        statsModel.getUserAchievementScore(req, item.classid, (errUserScore, resultUserScore) => {
+          if (errUserScore) console.error(errUserScore)
+
+          let index = resultUserScore.findIndex(x => x.userid === item.userid)
+          item.class_name = resultUserScore[index].name
+          item.message = `Peringkat ${index + 1} di Kelas ${item.class_name}`
+
+          let date = new Date(resultUserScore[index].created_at)
+          const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+          item.time = `${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`
+
+          next()
+        })
+      }, err => {
+        redisCache.setex(key, 81600, dataClass)
+        cb(err, dataClass)
+      })
+    }
+  ], (errUserScore, resultUserScore) => {
+    if (!errUserScore) {
+      return MiscHelper.responses(res, resultUserScore)
+    } else {
+      return MiscHelper.errorCustomStatus(res, errUserScore, 400)
     }
   })
 }
--- app/routes/v1/stats.js
+++ app/routes/v1/stats.js
@@ -8,5 +8,6 @@ Route
   .get('/get/:userId', StatsControllers.get)
   .get('/certificate/:userId/:classId', StatsControllers.getCertificate)
   .get('/certificate/:userId', StatsControllers.getCertificateList)
+  .get('/rank/:userId', StatsControllers.getRank)
 
 module.exports = Route
