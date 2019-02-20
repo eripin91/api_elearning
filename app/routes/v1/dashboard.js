b65776fd2aaec3a269eed3db9bd4a27fd95e9881
fitraaditama7 <fitraaditama7@gmail.com>
2019-02-20 15:29:17 +0700

    fix dashboard

--- app/routes/v1/dashboard.js
+++ app/routes/v1/dashboard.js
@@ -6,6 +6,6 @@ var Route = express.Router()
 
 Route
   .all('/*', AuthHelper.requiresAuthorization)
-  .get('/:userId', AuthHelper.requiresAccessToken, DashboardControllers.get)
+  .get('/:userId', DashboardControllers.get)
 
 module.exports = Route
