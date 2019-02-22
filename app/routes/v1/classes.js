ca18c47dc66fcdcd842222407a9339161744feb9
arham09 <arham.abiyan@gmail.com>
2019-02-22 14:43:32 +0700

    Fix auth class route

--- app/routes/v1/classes.js
+++ app/routes/v1/classes.js
@@ -5,11 +5,11 @@
 var Route = express.Router()
 
 Route
-  // .all('/*', AuthHelper.requiresAuthorization)
+  .all('/*', AuthHelper.requiresAuthorization)
   .get('/get', ClassesControllers.get)
   .get('/get/:classId/:userId', AuthHelper.requiresAccessToken, ClassesControllers.getDetail)
-  .get('/recs/:userId', ClassesControllers.getRec)
-  .get('/user/:userId', ClassesControllers.getUserClass)
+  .get('/recs/:userId', AuthHelper.requiresAccessToken, ClassesControllers.getRec)
+  .get('/user/:userId', AuthHelper.requiresAccessToken, ClassesControllers.getUserClass)
   .post('/rating', AuthHelper.requiresAccessToken, ClassesControllers.rating)
   .post('/add', ClassesControllers.insertUserClass)
 
